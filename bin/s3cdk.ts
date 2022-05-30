85975358e1039b711e58942cacae550ad54900c5
smehar <97095499+smehar@users.noreply.github.com>
2022-05-30 22:35:00 +0200

    fix comment

--- bin/s3cdk.ts
+++ bin/s3cdk.ts
@@ -6,18 +6,12 @@ import { S3CdkStackStorage } from '../lib/s3cdk-stackStorage';
 const app = new cdk.App();
 new S3CdkStackStorage(app, 'S3CdkStackStorage', {
 
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
+ 
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
 
-  env: { account: '972211976107', region: 'us-east-1' }
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
